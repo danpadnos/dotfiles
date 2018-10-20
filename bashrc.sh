d19f2c216fbcc40c58a3da994c09a2eb9d074242
Dan Padnos <danp@ai21.com>
2018-10-20 12:32:30 +0300

    added bash functions for running emacs

--- bashrc.sh
+++ bashrc.sh
@@ -13,8 +13,16 @@ export HISTCONTROL="ignoredups"
 export PROMPT_COMMAND="history -a"
 export HISTIGNORE="&:ls:[bf]g:exit"
 
-# Aliases
+# Aliases and functions
 alias ll='ls -lG'
+e()
+{
+    emacs "$@" &
+}
+enw()
+{
+    emacs -nw "$@"
+}
 
 # use a matplotlib backend suitable for OSX
 export MPLBACKEND="TkAgg"
